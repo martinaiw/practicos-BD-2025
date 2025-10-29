53c5a7d66b04b364abc96d8e04d448a1502192d1
Martina Ibañez Wulbrandt <martinaibanezw@mi.unc.edu.ar>
2025-10-29 00:43:09 -0300

    Ejercicio 11

--- P7/p7.js
+++ P7/p7.js
@@ -172,3 +172,29 @@ db.restaurants.find(
   },
   { restaurant_id: 1, grades: 1 }
 );
+
+//Ejercicio 11
+db.restaurants.updateOne(
+  { restaurant_id: "50018608" },
+  {
+    $push: {
+      grades: {
+        $each: [
+          {
+            date: ISODate("2019-10-10T00:00:00Z"),
+            grade: "A",
+            score: 18,
+            restaurant_id: "50018608",
+          },
+          {
+            date: ISODate("2020-02-25T00:00:00Z"),
+            grade: "A",
+            score: 21,
+            restaurant_id: "50018608",
+          },
+        ],
+      },
+    },
+  }
+);
+db.restaurants.findOne({ restaurant_id: "50018608" });
