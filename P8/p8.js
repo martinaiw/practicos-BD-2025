e766fea9d6b0aa9d6da9c1b30856c3e181cf5536
Martina Ibañez Wulbrandt <martinaibanezw@mi.unc.edu.ar>
2025-10-30 18:36:58 -0300

    Ejercicio 7

--- P8/p8.js
+++ P8/p8.js
@@ -7,29 +7,6 @@ db.theaters.aggregate([
   { $sort: { state: 1 } },
 ]);
 
-/*
-Ratings de IMDB promedio, mínimo y máximo por año de las películas estrenadas en los años 80 (desde 1980 hasta 1989), ordenados de mayor a menor por promedio del año.
-Título, año y cantidad de comentarios de las 10 películas con más comentarios.
-Crear una vista con los 5 géneros con mayor cantidad de comentarios, junto con la cantidad de comentarios.
-Listar los actores (cast) que trabajaron en 2 o más películas dirigidas por "Jules Bass". Devolver el nombre de estos actores junto con la lista de películas (solo título y año) dirigidas por “Jules Bass” en las que trabajaron. 
-Hint1: addToSet
-Hint2: {'name.2': {$exists: true}} permite filtrar arrays con al menos 2 elementos, entender por qué.
-Hint3: Puede que tu solución no use Hint1 ni Hint2 e igualmente sea correcta
-Listar los usuarios que realizaron comentarios durante el mismo mes de lanzamiento de la película comentada, mostrando Nombre, Email, fecha del comentario, título de la película, fecha de lanzamiento. HINT: usar $lookup con multiple condiciones 
-Listar el id y nombre de los restaurantes junto con su puntuación máxima, mínima y la suma total. Se puede asumir que el restaurant_id es único.
-Resolver con $group y accumulators.
-Resolver con expresiones sobre arreglos (por ejemplo, $sum) pero sin $group.
-Resolver como en el punto b) pero usar $reduce para calcular la puntuación total.
-Resolver con find.
-Actualizar los datos de los restaurantes añadiendo dos campos nuevos. 
-"average_score": con la puntuación promedio
-"grade": con "A" si "average_score" está entre 0 y 13, 
-  con "B" si "average_score" está entre 14 y 27 
-  con "C" si "average_score" es mayor o igual a 28    
-Se debe actualizar con una sola query.
-HINT1. Se puede usar pipeline de agregación con la operación update
-HINT2. El operador $switch o $cond pueden ser de ayuda.
-*/
 // Ejercicio 2 - Cantidad de estados con al menos dos cines (theaters) registrados.
 db.theaters.aggregate([
   { $group: { _id: "$location.address.state", count: { $sum: 1 } } },
@@ -73,9 +50,76 @@ db.comments.aggregate([
   {
     $project: {
       _id: 0,
-      "Nombre": "$_id.name",
-      "Email": "$_id.email",
+      Nombre: "$_id.name",
+      Email: "$_id.email",
       "Cantidad de comentarios": "$count",
     },
   },
 ]);
+
+// Ejercicio 7 - Ratings de IMDB promedio, mínimo y máximo
+// por año de las películas estrenadas en los años 80 (desde 1980 hasta 1989),
+// ordenados de mayor a menor por promedio del año.
+db.movies.aggregate([
+  {
+    $match: { year: { $gte: 1980, $lte: 1989 }, "imdb.rating": { $ne: null } },
+  },
+  {
+    $group: {
+      _id: "$year", //agrupa por año las estadisticas
+      promedio: { $avg: "$imdb.rating" },
+      minimo: { $min: "$imdb.rating" },
+      maximo: { $max: "$imdb.rating" },
+    },
+  },
+  { $sort: { promedio: -1 } },
+]);
+
+//Ejercicio 8 - Título, año y cantidad de comentarios de las 10 películas con más comentarios.
+db.movies.aggregate([
+  {
+    $lookup: {
+      from: "comments",
+      localField: "_id",
+      foreignField: "movie_id",
+      as: "movie_comments",
+    },
+  },
+  {
+    $addFields: {
+      commentCount: { $size: "$movie_comments" },
+    },
+  },
+  { $sort: { commentCount: -1 } },
+  { $limit: 10 },
+  {
+    $project: {
+      _id: 0,
+      Titulo: "$title",
+      Año: "$year",
+      "Cantidad de comentarios": "$commentCount",
+    },
+  },
+]);
+
+/*
+Crear una vista con los 5 géneros con mayor cantidad de comentarios, junto con la cantidad de comentarios.
+Listar los actores (cast) que trabajaron en 2 o más películas dirigidas por "Jules Bass". Devolver el nombre de estos actores junto con la lista de películas (solo título y año) dirigidas por “Jules Bass” en las que trabajaron. 
+Hint1: addToSet
+Hint2: {'name.2': {$exists: true}} permite filtrar arrays con al menos 2 elementos, entender por qué.
+Hint3: Puede que tu solución no use Hint1 ni Hint2 e igualmente sea correcta
+Listar los usuarios que realizaron comentarios durante el mismo mes de lanzamiento de la película comentada, mostrando Nombre, Email, fecha del comentario, título de la película, fecha de lanzamiento. HINT: usar $lookup con multiple condiciones 
+Listar el id y nombre de los restaurantes junto con su puntuación máxima, mínima y la suma total. Se puede asumir que el restaurant_id es único.
+Resolver con $group y accumulators.
+Resolver con expresiones sobre arreglos (por ejemplo, $sum) pero sin $group.
+Resolver como en el punto b) pero usar $reduce para calcular la puntuación total.
+Resolver con find.
+Actualizar los datos de los restaurantes añadiendo dos campos nuevos. 
+"average_score": con la puntuación promedio
+"grade": con "A" si "average_score" está entre 0 y 13, 
+  con "B" si "average_score" está entre 14 y 27 
+  con "C" si "average_score" es mayor o igual a 28    
+Se debe actualizar con una sola query.
+HINT1. Se puede usar pipeline de agregación con la operación update
+HINT2. El operador $switch o $cond pueden ser de ayuda.
+*/
