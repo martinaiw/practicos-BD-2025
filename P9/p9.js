b88c4cd9cb596bc64cc0af2120ca6b724a04e8c9
Martina Ibañez Wulbrandt <martinaibanezw@mi.unc.edu.ar>
2025-10-31 11:46:55 -0300

    Ejercicio 6 y 7

--- P9/p9.js
+++ P9/p9.js
@@ -156,13 +156,13 @@ db.runCommand({
       bsonType: "object",
       required: ["title", "year"],
       properties: {
-        title: { 
-          bsonType: "string" 
+        title: {
+          bsonType: "string",
         },
-        year: { 
-          bsonType: "int", 
-          minimum: 1900, 
-          maximum: 3000 
+        year: {
+          bsonType: "int",
+          minimum: 1900,
+          maximum: 3000,
         },
         cast: {
           bsonType: "array",
@@ -211,12 +211,151 @@ db.createCollection("userProfiles", {
   },
 });
 
-
 //MODELADO DE DATOS
-// Ejercicio 6 - Identificar los distintos tipos de relaciones (One-To-One, One-To-Many) en 
-// las colecciones movies y comments. Determinar si se usó documentos anidados o referencias 
+// Ejercicio 6 - Identificar los distintos tipos de relaciones (One-To-One, One-To-Many) en
+// las colecciones movies y comments. Determinar si se usó documentos anidados o referencias
 // en cada relación y justificar la razón.
 
+/* 
+MOVIES
+{
+  _id: ObjectId('573a1390f29313caabcd4132'),
+  title: 'Carmencita',
+  year: 1894,
+  runtime: 1,
+  cast: [ 'Carmencita' ],
+  poster: 'http://ia.media-imdb.com',
+  plot: 'Performing on what looks like a small wooden stage' 
+  fullplot: 'Performing on what looks like a small' 
+  lastupdated: '2015-08-26 00:03:45.040000000',
+  type: 'movie',
+  directors: [ 'William K.L. Dickson' ],
+  imdb: { rating: 5.9, votes: 1032, id: 1 },
+  countries: [ 'USA' ],
+  rated: 'NOT RATED',
+  genres: [ 'Documentary', 'Short' ]
+}
 
+COMMENTS
+{
+  _id: ObjectId('5a9427648b0beebeb69579cc'),
+  name: 'Andrea Le',
+  email: 'andrea_le@fakegmail.com',
+  movie_id: ObjectId('573a1390f29313caabcd418c'),
+  text: 'Rem officiis eaque repellendus amet eos doloribus. Porro dolor voluptatum voluptates neque culpa molestias. Voluptate unde nulla temporibus ullam.',
+  date: ISODate('2012-03-26T23:20:16.000Z')
+} 
+Movies - Comments (One-To-Many), una pelicula puede tener muchos comentarios pero un comentario pertenece a una sola pelicula
+Se usa una referencia al id de la pelicula en lugar de guardar todos los comentarios dentro de cada película
+*/
 
-// Ejercicio 7 - Dado el diagrama de la base de datos shop junto con las queries más importantes.
+// Ejercicio 7 - Dado el diagrama de la base de datos shop junto con las
+// queries más importantes.
+// Queries
+//  1- Listar el id, titulo, y precio de los libros y sus categorías de un autor en particular
+//  2- Cantidad de libros por categorías
+//  3- Listar el nombre y dirección entrega y el monto total
+//     (quantity * price) de sus pedidos para un order_id dado.
+
+// Crear el modelo de datos aplicndo modelado aniado y referencias.
+// Se deben responder las queries de manera eficiente: eficiente => no usar $lookup
+
+/* Colecciones
+orders: id, delivery_name, delivery_address, cc_name, cc_number, cc_expiry
+order_details: id, book_id (books), title, author, quantity, price, order_id (orders)
+books: book_id, title, author, price, category_id (category)
+categories: category_id, category_name
+
+*/
+
+use("shop");
+
+db.createCollection("orders", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        order_id: { bsonType: "long" },
+        delivery_name: { bsonType: "string", maxLength: 70 },
+        delivery_address: { bsonType: "string", maxLength: 70 },
+        cc_name: { bsonType: "string", maxLength: 70 },
+        cc_number: { bsonType: "string", maxLength: 32 },
+        cc_expiry: { bsonType: "string", maxLength: 20 },
+      },
+    },
+  },
+});
+db.createCollection("order_details", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        id: { bsonType: "long" },
+        book_id: { bsonType: "int" },
+        title: { bsonType: "string", maxLength: 70 },
+        author: { bsonType: "string", maxLength: 70 },
+        quantity: { bsonType: "int" },
+        price: { bsonType: "double" },
+        order_id: { bsonType: "long" },
+      },
+    },
+  },
+});
+db.createCollection("books", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        book_id: { bsonType: "int" },
+        title: { bsonType: "string", maxLength: 70 },
+        author: { bsonType: "string", maxLength: 70 },
+        price: { bsonType: "double" },
+        categories: { bsonType: "array", items: { bsonType: "string" } },
+      },
+    },
+  },
+});
+
+db.books.find(
+  { author: "George Orwell" },
+  { _id: 0, book_id: 1, title: 1, price: 1, categories: 1 }
+);
+
+db.books.aggregate([
+  { $unwind: "$categories" },
+  { $group: { _id: "$categories", count: { $sum: 1 } } },
+  { $project: { _id: 0, Categorias: "$_id", Cantidad: "$count" } },
+]);
+
+db.orders.aggregate([
+  {
+    $lookup: {
+      from: "order_details",
+      localField: "order_id",
+      foreignField: "order_id",
+      as: "order_details",
+    },
+  },
+  { $unwind: "$order_details" },
+  {
+    $group: {
+      _id: "$order_id",
+      delivery_name: { $first: "$delivery_name" },
+      delivery_address: { $first: "$delivery_address" },
+      costo: {
+        $sum: {
+          $multiply: ["$order_details.quantity", "$order_details.price"],
+        },
+      }, //multiply no acumula, x eso usamos sum
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      order_id: "$_id",
+      delivery_name: 1,
+      delivery_address: 1,
+      costo: 1,
+    },
+  },
+]);
