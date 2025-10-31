f30faa9a416116bf8b7feb904e7f6d5a89c76c46
Martina Ibañez Wulbrandt <martinaibanezw@mi.unc.edu.ar>
2025-10-31 02:10:34 -0300

    Ejercicio 2 y 3

--- P9/p9.js
+++ P9/p9.js
@@ -27,14 +27,122 @@ db.runCommand({
   },
 });
 
-// Ejercicio 2 - Obtener metadata de la colección users que garantice que las reglas 
+db.users.insertMany(
+  // 5 INSERTS VÁLIDOS
+  {
+    name: "Alex Smith",
+    email: "alex.smith@empresa.com",
+    password: "p".repeat(50),
+  },
+  {
+    name: "Maria Lopez",
+    email: "maria@mail.net",
+    password: "clavemuysegura".repeat(4) + "12345678901234",
+  },
+  {
+    name: "Carlos Gomez",
+    email: "carlos.gomez@sitio.co",
+    password:
+      "estaEsUnaClaveSuperLargaYSeguraParaCumplirConElMinimoDeCincuentaCaracteres",
+  },
+  {
+    name: "Daniela Rodriguez",
+    email: "daniela.r@servidor.info",
+    password: "a".repeat(30) + "b".repeat(20),
+  },
+  {
+    name: "Elias Torres",
+    email: "elias@torres.ar",
+    password:
+      "UnaContrasenaQueDebeTenerMasDeCincuentaCaracteresParaSerValidaEnLaBaseDeDatosDeUsuarios2025!",
+  },
+
+  // 5 INSERTS INVÁLIDOS
+  {
+    name: "Fanny",
+    email: "fanny@correo",
+    password: "valida".repeat(10),
+  },
+  {
+    name: "Gaston",
+    email: "gaston@correo.es",
+    password: "p".repeat(49),
+  },
+  {
+    name: "EsteNombreTieneMasDe30Caracteres",
+    email: "largo@correo.com",
+    password: "valida".repeat(10),
+  },
+  {
+    name: "Hugo",
+    password: "valida".repeat(10),
+  },
+  {
+    email: "invalido@gmail.com",
+    password: "valida".repeat(10),
+  }
+);
+
+// Ejercicio 2 - Obtener metadata de la colección users que garantice que las reglas
 // de validación fueron correctamente aplicadas.
+db.getCollectionInfos({ name: "users" });
 
+// Ejercicio 3 - Especificar en la colección theaters las siguientes reglas de validación:
+// El campo theaterId (requerido) debe ser un int y location (requerido) debe ser un object con:
+//      a - un campo address (requerido) que sea un object con campos street1, city, state y
+//          zipcode todos de tipo string y requeridos
+//      b - un campo geo (no requerido) que sea un object con un campo type, con valores
+//          posibles “Point” o null y coordinates que debe ser una lista de 2 doubles
+// Por último, estas reglas de validación no deben prohibir la inserción o actualización de
+// documentos que no las cumplan sino que solamente deben advertir.
 
-// Ejercicio 3 - Especificar en la colección theaters las siguientes reglas de validación: El campo theaterId (requerido) debe ser un int y location (requerido) debe ser un object con:
-// un campo address (requerido) que sea un object con campos street1, city, state y zipcode todos de tipo string y requeridos
-// un campo geo (no requerido) que sea un object con un campo type, con valores posibles “Point” o null y coordinates que debe ser una lista de 2 doubles
-// Por último, estas reglas de validación no deben prohibir la inserción o actualización de documentos que no las cumplan sino que solamente deben advertir.
-// Especificar en la colección movies las siguientes reglas de validación: El campo title (requerido) es de tipo string, year (requerido) int con mínimo en 1900 y máximo en 3000, y que tanto cast, directors, countries, como genres sean arrays de strings sin duplicados.
+db.runCommand({
+  collMod: "theaters",
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["theaterId", "location"],
+      properties: {
+        theaterId: {
+          bsonType: "int",
+        },
+        location: {
+          bsonType: "object",
+          required: ["address"],
+          properties: {
+            address: {
+              bsonType: "object",
+              required: ["street1", "city", "state", "zipcode"],
+              properties: {
+                street1: { bsonType: "string" },
+                city: { bsonType: "string" },
+                state: { bsonType: "string" },
+                zipcode: { bsonType: "string" },
+              },
+            },
+            geo: {
+              bsonType: "object",
+              properties: {
+                type: {
+                  enum: ["Point", null],
+                },
+                coordinates: {
+                  bsonType: "array",
+                  items: { bsonType: "double" },
+                  minItems: 2,
+                  maxItems: 2,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  validationLevel: "moderate",
+  validationAction: "warn",
+});
+
+// Ejercicio 4 - Especificar en la colección movies las siguientes reglas de validación: El campo title (requerido) es de tipo string, year (requerido) int con mínimo en 1900 y máximo en 3000, y que tanto cast, directors, countries, como genres sean arrays de strings sin duplicados.
 // Hint: Usar el constructor NumberInt() para especificar valores enteros a la hora de insertar documentos. Recordar que mongo shell es un intérprete javascript y en javascript los literales numéricos son de tipo Number (double).
-// Crear una colección userProfiles con las siguientes reglas de validación: Tenga un campo user_id (requerido) de tipo “objectId”, un campo language (requerido) con alguno de los siguientes valores [ “English”, “Spanish”, “Portuguese” ] y un campo favorite_genres (no requerido) que sea un array de strings sin duplicados.
+// Ejercicio 5 - Crear una colección userProfiles con las siguientes reglas de validación: Tenga un campo user_id (requerido) de tipo “objectId”, un campo language (requerido) con alguno de los siguientes valores [ “English”, “Spanish”, “Portuguese” ] y un campo favorite_genres (no requerido) que sea un array de strings sin duplicados.
